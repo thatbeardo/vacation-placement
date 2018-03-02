63a7d5adee61e7e7c96e841bb2dc60469ab369ce
DESKTOP-L2UTAJB\hmavani <hmavani@cimpress.com>
2018-03-02 20:46:36 +0530

    vue test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,11 @@
 <template>
   <v-app light>
-    <v-toolbar class="white">
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-    </v-toolbar>
     <v-content>
       <section>
         <v-parallax src="assets/hero.jpeg" height="600">
           <v-layout column align-center justify-center class="white--text">
             <img src="assets/vuetify.png" alt="Vuetify.js" height="200">
-            <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
+            <h1 class="white--text mb-2 display-1 text-xs-center">Campus To Corporate</h1>
             <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
             <v-btn class="blue lighten-2 mt-5" dark large href="/pre-made-themes">
               Get Started
@@ -21,7 +18,7 @@
         <v-layout column wrap class="my-5" align-center>
           <v-flex xs12 sm4 class="my-3">
             <div class="text-xs-center">
-              <h2 class="headline">The best way to start developing</h2>
+              <h2 class="headline">The best way to learn coding</h2>
               <span class="subheading">
                 Cras facilisis mi vitae nunc
               </span>
