9afa31f8c4949e9098401673b24edae7f8b1420e
DESKTOP-L2UTAJB\hmavani <hmavani@cimpress.com>
2018-03-03 12:01:00 +0530

    revert

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/vaccation-placement/',
       name: 'HelloWorld',
       component: HelloWorld
     }
