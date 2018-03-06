eaf4c2265ea91816acefb903aaaf19bb613e2578
DESKTOP-L2UTAJB\hmavani <hmavani@cimpress.com>
2018-03-06 16:15:02 +0530

    about us complete

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -11,7 +11,25 @@
       </section>
 
       <section>
-
+        <v-app light>
+          <v-container>
+            <v-layout row wrap align-center justify-space-around>
+              <v-flex xs12 md6 xl6>
+                <br><br>
+                <v-flex class="my-3" xs12 md12 xl12>
+                  <h2 class="headline blue--text">OUR CORE VALUES</h2>
+                  <blockquote class="text-xs-justify">
+                    {{values}}
+                  </blockquote>
+                </v-flex>
+                <v-flex v-for="instructor in instructors" :key=instructor.name>
+                  <instructor-card :instructorData="instructor"></instructor-card>
+                </v-flex>
+              </v-flex>
+            </v-layout>
+            <br><br><br><br><br>
+          </v-container>
+        </v-app>
       </section>
     </v-content>
   </v-app>
@@ -19,7 +37,24 @@
 </template>
 
 <script>
+import InstructorCard from "./InstructorCard";
+import instructors from "../data/instructors";
 export default {
-  name: "AboutUs"
+  name: "AboutUs",
+  data() {
+    return {
+      instructors: [],
+      values: `Incepted back in 2015, right after graduation, we have come a long way. It all began with one idea. To empower students with
+      a set of skills which enables them to select the company they want to work for and not the other way round. We witnessed tremendous success 
+      right from the first batch of C2C. Teaching students to code is our way of giving back to the society and fulfilling our social responsibility.
+      We believe that fundamental concepts which are essentially the key ingridients for a successful career are often over looked. This is 
+      where we step in. Having witnessed industry patterns, we know how to help you land a dream job. What makes us the right people for the job?
+      Well, we have secured dream jobs and compiled an encyclopedia of fundamental concepts most often asked in an interview`
+    };
+  },
+  created() {
+    this.instructors = instructors;
+  }
 };
 </script>
+
---
+++ src/components/InstructorCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-flex>
+    <br><br>
+    <h2 class="headline blue--text">{{ instructorData.name }}</h2>
+    <h2 class="subheading grey--text">{{ instructorData.designation }} at {{instructorData.company}}</h2>
+    <v-layout row wrap>
+      <v-flex xs12 md6 xl6>
+        <img :src="instructorData.image" alt="" width="400px">
+      </v-flex>
+      <v-flex xs12 md6 xl6 class="text-xs-justify">{{instructorData.about}}</v-flex>
+    </v-layout>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: "InstructorCard",
+  data() {
+    return {
+      imagePreUrl: "../assets/"
+    };
+  },
+  props: {
+    instructorData: {
+      type: Object
+    }
+  }
+};
+</script>
