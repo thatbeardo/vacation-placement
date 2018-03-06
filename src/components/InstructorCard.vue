bba438c69eedade4ab89a50bae37e952058f4e93
DESKTOP-L2UTAJB\hmavani <hmavani@cimpress.com>
2018-03-06 22:53:02 +0530

    google analytics and mobile bug

--- src/components/InstructorCard.vue
+++ src/components/InstructorCard.vue
@@ -3,12 +3,15 @@
     <br><br>
     <h2 class="headline blue--text">{{ instructorData.name }}</h2>
     <h2 class="subheading grey--text">{{ instructorData.designation }} at {{instructorData.company}}</h2>
-    <v-layout row wrap>
-      <v-flex xs12 md12 xl6>
-        <img :src="instructorData.image" alt="" width="400px">
-      </v-flex>
-      <v-flex xs12 md12 xl6 class="text-xs-justify">{{instructorData.about}}</v-flex>
-    </v-layout>
+    <v-container grid-list-md>
+      <v-layout row wrap justify-space-around>
+        <v-flex xs12 md12 xl6 justify-space-around>
+          <img :src="instructorData.image" alt="" width="400px">
+        </v-flex>
+        <v-flex xs12 md12 xl6 class="text-xs-justify">{{instructorData.about}}</v-flex>
+      </v-layout>
+    </v-container>
+
   </v-flex>
 </template>
 
