ad00c13ad79172913010eb570ab7d2b8f6988d8c
DESKTOP-L2UTAJB\hmavani <hmavani@cimpress.com>
2018-03-06 16:38:53 +0530

    Issue when viewed on md screens

--- src/components/InstructorCard.vue
+++ src/components/InstructorCard.vue
@@ -4,10 +4,10 @@
     <h2 class="headline blue--text">{{ instructorData.name }}</h2>
     <h2 class="subheading grey--text">{{ instructorData.designation }} at {{instructorData.company}}</h2>
     <v-layout row wrap>
-      <v-flex xs12 md6 xl6>
+      <v-flex xs12 md12 xl6>
         <img :src="instructorData.image" alt="" width="400px">
       </v-flex>
-      <v-flex xs12 md6 xl6 class="text-xs-justify">{{instructorData.about}}</v-flex>
+      <v-flex xs12 md12 xl6 class="text-xs-justify">{{instructorData.about}}</v-flex>
     </v-layout>
   </v-flex>
 </template>
