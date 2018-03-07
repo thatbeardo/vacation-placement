1c68a9e72348f4e9923ef0522a04bbcc076407ea
DESKTOP-L2UTAJB\hmavani <hmavani@cimpress.com>
2018-03-07 20:10:14 +0530

    Card animation, text edit

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -1,39 +1,41 @@
 
 <template>
-    <v-card>
-        <v-card-media :src="studentData.avatar" height=" 200px" contain>
-        </v-card-media>
-        <v-card-title primary-title>
-            <div>
-                <v-container fluid grid-list-xs>
-                    <v-layout row wrap>
-                        <v-flex xs12>
-                            <div class="headline ">{{ studentData.firstName }} {{ studentData.lastName }}</div>
-                            <span class="grey--text ">{{ studentData.designation }} at {{ studentData.company }} </span>
-                            <br>
-                            <span class="grey--text ">Campus to Corporate Batch of {{ studentData.year }}</span>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-            </div>
-        </v-card-title>
-        <v-flex xs12 md12>
-            <v-card-actions>
-                <v-btn flat color="blue " @click.native="show=! show ">{{ studentData.firstName }}'s take on C2C </v-btn>
-            </v-card-actions>
-            <v-dialog v-model="show" max-width="400">
-                <v-card>
-                    <v-card-title class="headline">Here's what {{studentData.firstName}} has to say</v-card-title>
-                    <v-card-text class="text-xs-justify">{{studentData.testimony}}</v-card-text>
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" flat="flat" @click.native="show = false">Close</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-dialog>
+    <div class="enlarge">
+        <v-card>
+            <v-card-media :src="studentData.avatar" height=" 200px" contain>
+            </v-card-media>
+            <v-card-title primary-title>
+                <div>
+                    <v-container fluid grid-list-xs>
+                        <v-layout row wrap>
+                            <v-flex xs12>
+                                <div class="headline ">{{ studentData.firstName }} {{ studentData.lastName }}</div>
+                                <span class="grey--text ">{{ studentData.designation }} at {{ studentData.company }} </span>
+                                <br>
+                                <span class="grey--text ">Campus to Corporate Batch of {{ studentData.year }}</span>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </div>
+            </v-card-title>
+            <v-flex xs12 md12>
+                <v-card-actions>
+                    <v-btn flat color="blue " @click.native="show=! show ">{{ studentData.firstName }}'s take on C2C </v-btn>
+                </v-card-actions>
+                <v-dialog v-model="show" max-width="400">
+                    <v-card>
+                        <v-card-title class="headline">Here's what {{studentData.firstName}} has to say</v-card-title>
+                        <v-card-text class="text-xs-justify">{{studentData.testimony}}</v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" flat="flat" @click.native="show = false">Close</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
 
-        </v-flex>
-    </v-card>
+            </v-flex>
+        </v-card>
+    </div>
 </template>
 
 <script>
@@ -51,3 +53,12 @@ export default {
   }
 };
 </script>
+
+<style>
+.enlarge {
+  transition: transform 0.2s ease-in-out;
+}
+.enlarge:hover {
+  transform: scale(1.05, 1.05);
+}
+</style>
