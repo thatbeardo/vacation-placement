b793fab8b8f19b95508ff15ad2f5dc118a631d56
DESKTOP-L2UTAJB\hmavani <hmavani@cimpress.com>
2018-05-06 14:13:26 +0530

    Typos fixed.
    Costing updated

--- src/components/CourseContent.vue
+++ src/components/CourseContent.vue
@@ -21,22 +21,16 @@
           </v-slide-y-transition>
         </v-card>
       </v-flex>
-      <v-flex xs10 sm10>
-        <h2 class="headline blue--text pl-3 pb-2">Pricing</h2>
-        <blockquote class="pl-3 pb-3">The course will be broken down into 11 sessions of 4 hours each. Hands on coding practice is ensured which is why machines are required. The entire course will cost you either 5000/- or 6500/- based on the availibility of a machine. Apart from coding, we will focus on developing soft skills. Every session will have a dedicated 10-15 mins slot for soft skills discussions. Also, we will have a separate session - 12th session to understand a few database concepts required for many dream company interviews</blockquote>
+      <v-flex xs12 sm12 md10 xl10>
+        <h2 class="headline blue--text pl-3 pb-2 ">Pricing</h2>
+        <blockquote class="pl-3 pb-3 text-xs-justify">The course will be broken down into 11 sessions of 4 hours each. Hands on coding practice is ensured which is why machines are required. The entire course will cost you either 5000/- or 6500/- based on the availibility of a machine. Apart from coding, we will focus on developing soft skills. Every session will have a dedicated 10-15 mins slot for soft skills discussions. Also, we will have a separate session - 12th session to understand a few database concepts required for many dream company interviews</blockquote>
         <v-list three-line subheader>
-          <v-list-tile>
+          <v-list-tile v-for="plan in pricing" :key="plan.cost">
             <v-list-tile-content>
               <v-list-tile-title>
-                <div class="subheading blue--text">INR 5000</div>
+                <div class="subheading blue--text">INR {{plan.cost}}/-</div>
               </v-list-tile-title>
-              <v-list-tile-sub-title>If you can get a laptop for each session</v-list-tile-sub-title>
-            </v-list-tile-content>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                <div class="subheading blue--text">INR 6500</div>
-              </v-list-tile-title>
-              <v-list-tile-sub-title>We provide you with machine for each session</v-list-tile-sub-title>
+              <v-list-tile-sub-title>{{plan.details}}</v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
@@ -52,6 +46,18 @@ export default {
   name: 'CourseContent',
   data() {
     return {
+      pricing: [
+        {
+          cost: '5000',
+          details:
+            'We need a machine for each session. We need you to understand that is not a theory course. We get our hands dirty and learn to code. Everyday. If you can get a laptop of your own, then it wil cost you Rs. 5000/-'
+        },
+        {
+          cost: '6500',
+          details:
+            'If you cannot for some reason, then you pay a little extra and we will provide you with a machine for each session. This will cost you Rs 6500/-'
+        }
+      ],
       courseContent: [],
       visible: []
     };
