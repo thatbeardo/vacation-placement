847ab8c7f0703253c53a34d11f07e0f021365ebf
DESKTOP-L2UTAJB\hmavani <hmavani@cimpress.com>
2018-05-06 16:01:38 +0530

    Maps Fix
    Maps responsive

--- src/components/CourseContent.vue
+++ src/components/CourseContent.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container grid-list-xl>
-    <v-layout row wrap justify-space-around>
+    <v-layout row wrap justify-space-around v-resize="onResize">
       <v-flex xs10 sm10>
         <v-card class="my-5" v-for="(session, index) in courseContent" :key="index">
           <v-card-media :src=session.image height="400px">
@@ -24,12 +24,12 @@
       <v-flex xs12 sm12 md10 xl10>
         <h2 class="headline blue--text pl-3 pb-2 ">Venue</h2>
         <blockquote class="pl-3 pb-3 text-xs-justify">
-          <GmapMap :center="{lat:19.119791, lng:72.845654}" :zoom="16" map-type-id="hybrid" style="width: 500px; height: 300px">
+          <GmapMap :center="{lat:19.119791, lng:72.845654}" :zoom="16" map-type-id="hybrid" :style=getDimensions()>
             <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position" />
           </GmapMap>
           <br> Kossine, 3rd Floor, BEST Commercial Complex, B - Building, Station Road, Andheri West, Mumbai, 400067 Landmark: Near McDonalds</blockquote>
         <h2 class="headline blue--text pl-3 pb-2 ">Pricing</h2>
-        <blockquote class="pl-3 pb-3 text-xs-justify">The course will be broken down into 11 sessions of 4 hours each. Hands on coding practice is ensured which is why machines are required. The entire course will cost you either 5000/- or 6500/- based on the availibility of a machine. Apart from coding, we will focus on developing soft skills. Every session will have a dedicated 10-15 mins slot for soft skills discussions. Also, we will have a separate session - 12th session to understand a few database concepts required for many dream company interviews</blockquote>
+        <blockquote class="pl-3 pb-3 text-xs-justify">The course will be broken down into 11 sessions of 4 hours each. Hands on coding practice is ensured which is why machines are required. We need a machine for each session. We need you to understand that is not a theory course. We get our hands dirty and learn to code. Also, we will have a separate session - 12th session to understand a few database concepts required for many dream company interviews</blockquote>
         <v-list three-line subheader>
           <v-list-tile v-for="plan in pricing" :key="plan.cost">
             <v-list-tile-content>
@@ -52,22 +52,29 @@ export default {
   name: 'CourseContent',
   data() {
     return {
+      width: 500,
       pricing: [
         {
           cost: '5000',
-          details:
-            'We need a machine for each session. We need you to understand that is not a theory course. We get our hands dirty and learn to code. Everyday. If you can get a laptop of your own, then it wil cost you Rs. 5000/-'
+          details: 'Incase you can get your own laptop'
         },
         {
           cost: '6500',
-          details:
-            'If you cannot get a laptop for some reason, then you pay a little extra and we will provide you with a machine for each session. This will cost you Rs 6500/-'
+          details: 'Incase you cannot bring your laptop'
         }
       ],
       courseContent: [],
       visible: [],
       markers: [{ position: { lat: 19.119791, lng: 72.845654 } }]
     };
+  },
+  methods: {
+    onResize() {
+      this.width = window.innerWidth - 50;
+    },
+    getDimensions() {
+      return `width: ${this.width}; height: 300px`;
+    }
   },
   created() {
     this.courseContent = courseContent;
