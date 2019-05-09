f87926e207fa6be565e1569384b08da4fa54b146
Harshil Mavani <hmavani@cimpress.com>
2019-05-09 11:34:04 -0400

    Updated map

--- src/components/CourseContent.vue
+++ src/components/CourseContent.vue
@@ -1,15 +1,33 @@
 <template>
   <v-container grid-list-xl>
-    <v-layout row wrap justify-space-around v-resize="onResize">
-      <v-flex xs10 sm10>
-        <v-card class="my-5" v-for="(session, index) in courseContent" :key="index">
-          <v-card-media :src=session.image height="400px">
+    <v-layout
+      row
+      wrap
+      justify-space-around
+      v-resize="onResize"
+    >
+      <v-flex
+        xs10
+        sm10
+      >
+        <v-card
+          class="my-5"
+          v-for="(session, index) in courseContent"
+          :key="index"
+        >
+          <v-card-media
+            :src=session.image
+            height="400px"
+          >
           </v-card-media>
           <v-card-title primary-title>
             <div>
               <div class="headline">{{session.title}}</div>
               <span class="grey--text">{{session.summary}}</span>
-              <v-btn icon @click.native="visible.includes(index)? visible.splice(visible.indexOf(index), 1): visible.push(index)">
+              <v-btn
+                icon
+                @click.native="visible.includes(index)? visible.splice(visible.indexOf(index), 1): visible.push(index)"
+              >
                 <v-icon>{{ visible.includes(index) ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
               </v-btn>
             </div>
@@ -21,17 +39,41 @@
           </v-slide-y-transition>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm12 md10 xl10>
+      <v-flex
+        xs12
+        sm12
+        md10
+        xl10
+      >
         <h2 class="headline blue--text pl-3 pb-2 ">Venue</h2>
         <blockquote class="pl-3 pb-3 text-xs-justify">
-          <GmapMap :center="{lat:19.119791, lng:72.845654}" :zoom="16" map-type-id="hybrid" :style=getDimensions()>
-            <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position" />
+          <GmapMap
+            :center="{lat: 19.0369, lng: 72.8584}"
+            :zoom="16"
+            map-type-id="hybrid"
+            :style=getDimensions()
+          >
+            <GmapMarker
+              :key="index"
+              v-for="(m, index) in markers"
+              :position="m.position"
+              :clickable="true"
+              :draggable="true"
+              @click="center=m.position"
+            />
           </GmapMap>
-          <br> Kossine, 3rd Floor, BEST Commercial Complex, B - Building, Station Road, Andheri West, Mumbai, 400067 Landmark: Near McDonalds</blockquote>
+          <br> 143 / B Padmalaya, 1st floor, Sion Jain Society. Mumbai 400022
+        </blockquote>
         <h2 class="headline blue--text pl-3 pb-2 ">Pricing</h2>
-        <blockquote class="pl-3 pb-3 text-xs-justify">The course will be broken down into 11 sessions of 4 hours each. Hands on coding practice is ensured which is why machines are required. We need a machine for each session. We need you to understand that is not a theory course. We get our hands dirty and learn to code. Also, we will have a separate session - 12th session to understand a few database concepts required for many dream company interviews</blockquote>
-        <v-list three-line subheader>
-          <v-list-tile v-for="plan in pricing" :key="plan.cost">
+        <blockquote class="pl-3 pb-3 text-xs-justify">The course will be broken down into 11 - 12 sessions of 3 hours each. Hands on coding practice is ensured which is why machines are required. We need a machine for each session. We need you to understand that is not a theory course. We get our hands dirty and learn to code. Also, we will have a separate session - 12th session to understand softskills required for many dream company interviews</blockquote>
+        <v-list
+          three-line
+          subheader
+        >
+          <v-list-tile
+            v-for="plan in pricing"
+            :key="plan.cost"
+          >
             <v-list-tile-content>
               <v-list-tile-title>
                 <div class="subheading blue--text">INR {{plan.cost}}/-</div>
@@ -46,26 +88,23 @@
 </template>
 
 <script>
-import * as courseContent from '../data/courseContent';
+import * as courseContent from "../data/courseContent";
 
 export default {
-  name: 'CourseContent',
+  name: "CourseContent",
   data() {
     return {
       width: 500,
       pricing: [
         {
-          cost: '5000',
-          details: 'Incase you can get your own laptop'
-        },
-        {
-          cost: '6500',
-          details: 'Incase you cannot bring your laptop'
+          cost: "5000",
+          details:
+            "You most defnitely need a laptop for this course. We will experience coding in all its glory."
         }
       ],
       courseContent: [],
       visible: [],
-      markers: [{ position: { lat: 19.119791, lng: 72.845654 } }]
+      markers: [{ position: { lat: 19.0369, lng: 72.8584 } }]
     };
   },
   methods: {
